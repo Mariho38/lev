38e7e02cd2b7781bcfc24cdd801111f239ffdfb1
Mariho38 <happy0213happy@outlook.jp>
2021-05-06 18:38:02 +0900

    [add]バリデーション追加

---
+++ resources/views/create.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>blog</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        <form action="/posts" method="POST">
+             
+             @csrf
+                   
+             <div class="title">
+                 
+                <h2>Titile</h2>
+                <input type="text" name="post[title]" placeholder="タイトル" value="{{ old('post.title') }}"/>
+                 <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
+            </div>
+            <div class="body">
+                <h2>Body</h2>
+                <textarea name="post[body]" placeholder="今日もお疲れ様でした。">{{ old('post.body') }}</textarea>
+                <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
+            </div>
+            <input type="submit" value="保存"/>    
+         </form> 
+
+        <div class='back'>[<a href='/'>back</a>]</div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,7 @@ use Illuminate\Support\Facades\Routes;
 
 
 Route::get('/', 'PostController@index');
-Route::get('/post/{post}', 'PostController@show');
+Route::get('/posts/create', 'PostController@create');
+Route::get('/posts/{post}', 'PostController@show');
+
+Route::post('/posts', 'PostController@store');
